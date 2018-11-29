7c7d16feda0f42bf0aa862baf928b8ea71b27023
Trent Boyd <trent.boyd@gmail.com>
2018-11-29 12:25:27 -0600

    exercise 1

--- 01-imperative-to-declarative/exercise/src/App.start.js
+++ 01-imperative-to-declarative/exercise/src/App.start.js
@@ -11,26 +11,46 @@ Goal: Update the document title to say "Todos ([incomplete])" as the data change
 Tips:
 
 - You'll need two lifecycle hooks
-- You'll need string interpolation `it looks ${like} this`
+- You'll need string interpol ation `it looks ${like} this`
 - the DOM API to update the title is `document.title = "some string"`
 
 */
 
 import React, { Component } from "react";
 
+class DocumentTitle extends Component {
+  componentDidMount() {
+    this.setTitle(this.props.title);
+  }
+
+  componentDidUpdate() {
+    this.setTitle(this.props.title);
+  }
+
+  setTitle() {
+    document.title = this.props.title;
+  }
+
+  render() {
+    return null;
+  }
+}
+
 class App extends Component {
   state = {
     completed: 0,
-    todos: ["Wake up", "Eat a taco", "Avoid twitter"]
+    todos: ["Wake up", "Eat a taco", "Avoid twitter"],
+    title: "Todos"
   };
 
   render() {
     let { todos, completed } = this.state;
-    let incomplete = todos.length - completed;
 
     return (
       <div className="app">
-        <h1>Todos ({incomplete})</h1>
+        <DocumentTitle title={`Todos (${todos.length - completed})`} />
+
+        <h1>Todos ({todos.length - completed})</h1>
 
         <form
           onSubmit={event => {
